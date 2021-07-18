389baaeb8b54380bb99904969c2d5f24b8c9bd79
Chitramaddy <chitramaddy@gmail.com>
2021-07-18 18:31:26 -0400

    react basic form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import Posts from './components/Posts';
+import Postform from './components/Postform';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to my posts
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       </header>
+       <Postform />
+       <Posts />
+      
     </div>
   );
 }
---
+++ src/components/Posts.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+class Posts extends Component {
+  state = {
+    posts: [],
+  };
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((data) =>
+        this.setState({
+          posts: data,
+        })
+      );
+  }
+  render() {
+    const postItems = this.state.posts.map((post) => (
+      <div key={post.id}>
+        <h4>{post.title}</h4>
+        <p>{post.body}</p>
+      </div>
+    ));
+    return <div>{ postItems }</div>;
+  }
+}
+
+export default Posts;
